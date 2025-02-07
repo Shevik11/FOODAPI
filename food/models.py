989fbd8d6af76ffb140f9a244f8e16651af84544
Shevik11 <maksshevchuk621@gmail.com>
2025-02-07 15:59:41 +0200

    CRUD realisation

--- food/models.py
+++ food/models.py
@@ -5,6 +5,6 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
     price = models.DecimalField(max_digits=5, decimal_places=2)
-
+    image = models.ImageField(upload_to="images/", default="images/food.jpg")
     def __str__(self):
         return self.name
--- food/urls.py
+++ food/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
 
+
 app_name = 'food'
 
 urlpatterns = [
     path('<int:item_id>/', views.detail, name='detail'),
     path('', views.index, name='index'),
+    path('add/', views.create_item, name='create_item'),
+    path('update/<int:id>/', views.update_item, name='update_item'),
+    path('delete/<int:id>/', views.delete_item, name='delete_item'),
 ]
+
