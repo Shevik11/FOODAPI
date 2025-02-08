28e30db73e0bd28194595d7f7c7d27a8d7c5778e
Shevik11 <maksshevchuk621@gmail.com>
2025-02-08 11:16:15 +0200

    add profile page and create login/logout

--- FOODAPI/settings.py
+++ FOODAPI/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "images")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+LOGIN_REDIRECT_URL = 'food:index'
+LOGIN_URL = 'login'
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
